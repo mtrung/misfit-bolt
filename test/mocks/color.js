04111b0687f2f7a1b1a63b705ba14fdda0e3f983
flochtililoch <flochtililoch@gmail.com>
2016-07-09 12:48:39 -0700

    Improve alpha handling, add integration test and remove outdated tests.

--- test/mocks/color.js
+++ test/mocks/color.js
@@ -10,6 +10,14 @@ class Color {
     return {r: 255, g: 255, b: 255}
   }
 
+  alpha(value) {
+    if (value !== undefined) {
+      this._alpha = value;
+    } else {
+      return 1;
+    }
+  }
+
 }
 
 module.exports = Color;
--- test/state.test.js
+++ test/state.test.js
@@ -32,6 +32,36 @@ describe('State', () => {
 
   });
 
+  describe('#buffer', () => {
+
+    var buffer = new Buffer('foo');
+
+    describe('getter', () => {
+
+      before(() => {
+        state._buffer = buffer;
+      });
+
+      it('retrieves internal buffer', () => {
+        chai.expect(state.buffer.toString()).to.equal(buffer.toString());
+      });
+
+    });
+
+    describe('setter', () => {
+
+      before(() => {
+        state.buffer = buffer;
+      });
+
+      it('sets internal buffer', () => {
+        chai.expect(state._buffer.toString()).to.equal(buffer.toString());
+      });
+
+    });
+
+  });
+
   describe('#value', () => {
 
     describe('getter', () => {
@@ -82,18 +112,16 @@ describe('State', () => {
 
   });
 
-  describe('#buffer', () => {
+  describe('#color', () => {
 
-    var buffer = new Buffer('foo');
+    before(() => {
+      state.value = "abc";
+    });
 
     describe('getter', () => {
 
-      before(() => {
-        state._buffer = buffer;
-      });
-
-      it('retrieves internal buffer', () => {
-        chai.expect(state.buffer.toString()).to.equal(buffer.toString());
+      it('returns an instance of color initialized with the right params', () => {
+        chai.expect(state.color.args[0]).to.equal('rgba(abc)');
       });
 
     });
@@ -101,243 +129,12 @@ describe('State', () => {
     describe('setter', () => {
 
       before(() => {
-        state.buffer = buffer;
+        state.color = new Color();
       });
 
-      it('sets internal buffer', () => {
-        chai.expect(state._buffer.toString()).to.equal(buffer.toString());
+      it('correctly sets the state value', () => {
+        chai.expect(state.value).to.equal('255,255,255,100')
       });
-
-    });
-
-  });
-
-  describe('#rgba', () => {
-
-    describe('getter', () => {
-
-      describe('when value matches expected format', () => {
-
-        before(() => {
-          state.value = "123,456,789,012";
-        });
-
-        it('extracts separated colors and luminosity as an array', () => {
-          chai.expect(state.rgba).to.deep.equal([123, 456, 789, 12]);
-        });
-
-      });
-
-      describe('when value does not match expected format and is recognized as `on` value', () => {
-
-        before(() => {
-          state.value = "foobarbaz";
-        });
-
-        it('returns default separated colors and full luminosity as an array', () => {
-          chai.expect(state.rgba).to.deep.equal([255, 255, 255, 100]);
-        });
-
-      });
-
-      describe('when value does not match expected format and is not recognized as `on` value', () => {
-
-        before(() => {
-          state.value = "foobarbaz,0";
-        });
-
-        it('returns default separated colors and min luminosity as an array', () => {
-          chai.expect(state.rgba).to.deep.equal([255, 255, 255, 0]);
-        });
-
-      });
-
-    });
-
-    describe('setter', () => {
-
-      describe('when red is not in [0, 255] range', () => {
-        it('throws an error', () => {
-          chai.expect(
-            () => state.rgba = [256,0,0,0]
-          ).to.throw(/State, set rgba : red should be an integer between 0 and 255/);
-        });
-      });
-
-      describe('when green is not in [0, 255] range', () => {
-        it('throws an error', () => {
-          chai.expect(
-            () => state.rgba = [0,256,0,0]
-          ).to.throw(/State, set rgba : green should be an integer between 0 and 255/);
-        });
-      });
-
-      describe('when blue is not in [0, 255] range', () => {
-        it('throws an error', () => {
-          chai.expect(
-            () => state.rgba = [0,0,256,0]
-          ).to.throw(/State, set rgba : blue should be an integer between 0 and 255/);
-        });
-      });
-
-      describe('when alpha is not in [0, 100] range', () => {
-        it('throws an error', () => {
-          chai.expect(
-            () => state.rgba = [0,0,0,101]
-          ).to.throw(/State, set rgba : alpha should be an integer between 0 and 100/);
-        });
-      });
-
-      describe('when rgba is valid', () => {
-
-        var rgba = [100,100,100,100];
-
-        before(() => {
-          state.rgba = rgba;
-        });
-
-        it('sets value', () => {
-          chai.expect(state.value).to.equal(rgba.join(','));
-        });
-
-      });
-
-    });
-
-  });
-
-  describe('Color related methods', () => {
-
-    var color;
-
-    describe('#color', () => {
-
-      describe('getter', () => {
-
-        const rgba = [1, 2, 3, 4];
-
-        before(() => {
-          state.rgba = rgba;
-          color = state.color;
-        });
-
-        it('returns an instance of `Color`', () => {
-          chai.expect(color).to.be.instanceof(Color);
-        });
-
-        it('is instanciated with value of rgba', () => {
-          chai.expect(color.args.length).to.equal(1);
-          chai.expect(color.args[0]).to.equal(`rgba(${rgba.join(',')})`);
-        });
-
-      });
-
-      describe('setter', () => {
-
-        const rgb = {r:5, g:6, b:7};
-
-        before(() => {
-          color = new Color();
-          spy = sandbox.stub(color, 'rgb', () => { return rgb; });
-          state.brightness = 32;
-          state.color = color;
-        });
-
-        it('sets rgba property using rgb from color', () => {
-          chai.expect(spy.calledOnce).to.be.true;
-        });
-
-        it('sets rgba property with color details and internal brightness', () => {
-          chai.expect(state.rgba).to.deep.equal([rgb.r, rgb.g, rgb.b,state.brightness]);
-        });
-
-      });
-
-    });
-
-    describe('#hue', () => {
-
-      const hue = "hue";
-
-      describe('getter', () => {
-
-        var returnedHue;
-
-        before(() => {
-          spy = sandbox.stub(Color.prototype, 'hue', () => {
-            return hue;
-          });
-          returnedHue = state.hue;
-        });
-
-        it('returns value from color.hue', () => {
-          chai.expect(spy.calledOnce).to.be.true;
-          chai.expect(returnedHue).to.equal(hue);
-        });
-
-      });
-
-      describe('setter', () => {
-
-        // TODO
-
-      });
-
-    });
-
-    describe('#saturation', () => {
-
-      const saturation = "saturation";
-
-      describe('getter', () => {
-
-        var returnedSaturation;
-
-        before(() => {
-          spy = sandbox.stub(Color.prototype, 'saturation', () => {
-            return saturation;
-          });
-          returnedSaturation = state.saturation;
-        });
-
-        it('returns value from color.saturation', () => {
-          chai.expect(spy.calledOnce).to.be.true;
-          chai.expect(returnedSaturation).to.equal(saturation);
-        });
-
-      });
-
-      describe('setter', () => {
-
-        // TODO
-
-      });
-
-    });
-
-  });
-
-  describe('#brightness', () => {
-
-    describe('getter', () => {
-
-      const rgba = [0, 0, 0, 27];
-      var returnedBrightness;
-
-      before(() => {
-        state.rgba = rgba;
-        returnedBrightness = state.brightness;
-      });
-
-      it('returns last value of rgba array', () => {
-        chai.expect(returnedBrightness).to.equal(rgba[3]);
-      });
-
-    });
-
-    describe('setter', () => {
-
-      // TODO
 
     });
 
